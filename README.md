c404018ca376064e75140199da04fc187f7e78da
Praneeth <praneeth.vad@gmail.com>
2024-09-29 19:49:31 +0530

    Uploaded files

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# LML
+<!-- Copyright (c) 2024 Praneeth Vadlapati -->
+
+# <img src="./files/logo_small.jpg" align="left" width="250" alt="LML-DAP" /> LML & DAP
+
+## Language Model Learning a Dataset for Data-Augmented Prediction
+
+[![License: AFL v3](https://img.shields.io/badge/License-AFLv3-yellow.svg?style=for-the-badge)](./LICENSE.md)
+[![arXiv](https://img.shields.io/badge/arXiv-<arxiv id here>-B31B1B?logo=arxiv&style=for-the-badge)](https://arxiv.org/abs/<arxiv id here>)
+[![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=ffdd54)](https://www.python.org/)
+
+<br> <br>
+
+#### Why LML & DAP?
+- Classification tasks are typically handled using Machine Learning (ML) models, which lack a balance between accuracy and interpretability.
+- This project introduces a new approach to using Large Language Models (LLMs) for classification tasks in an explainable way.
+- Unlike ML models that rely heavily on data cleaning and feature engineering, this method streamlines the process using LLMs.
+
+<!-- Demo image -->
+![Demo](./files/Demo.png)
+
+Created by Praneeth Vadlapati ([@prane-eth](https://github.com/prane-eth))
+
+> [!NOTE]
+> Please star :star: the repository to show your support. <br>
+
+## :page_facing_up: Research Paper
+A **preprint** of the research paper is available on [arXiv](https://arxiv.org/abs/<arxiv id here>) <br>
+
+## :bookmark_tabs: Citation
+To use my paper for reference, please cite it as below:
+```bibtex
+@misc{vadlapati2024lml,
+	title={{LML: Language Model Learning a Dataset for Data-Augmented Prediction}},
+	author={{Praneeth Vadlapati}},
+	year={2024},
+	month={Sep},
+	archivePrefix={arXiv},
+	primaryClass={cs.CL},
+	url={https://arxiv.org/abs/<arxiv id here>}
+}
+```
+
+
+## :rocket: Quick Start
+```bash
+pip install -r requirements.txt
+cp .env.example .env
+```
+Now, edit the `.env` file and add your values. <br>
+Run the file [Experiment-LML.ipynb](Experiment-LML.ipynb)
+
+
+## :computer: More Projects
+For more projects, open the profile: **[@Pro-GenAI](https://github.com/Pro-GenAI)** <br>
+
+
+## :hammer_and_wrench: Contributing
+Contributions are welcome! Feel free to create an issue for any bug reports or suggestions. <br>
+To contribute, star :star: the repository and create an Issue. If I can't solve it, I will allow anyone to create a pull request.<br>
+
+
+## :identification_card: License
+Copyright (c) 2024 Praneeth Vadlapati <br>
+Please refer to the [LICENSE](./LICENSE.md) file for more information.
+
+
+## :warning: Disclaimer
+The code is not intended for use in production environments.
+This code is for educational and research purposes only.
+
+No author is responsible for any misuse or damage caused by this code.
+Use it at your own risk. The code is provided as is without any guarantees or warranty.
+
+## :globe_with_meridians: Acknowledgements
+- Special thanks to **Groq** (https://groq.com/) for a fast a inference of Llama 3.1
+
+
+## :email: Contact
+For personal queries, please find my contact details here: [linktr.ee/prane.eth](https://linktr.ee/prane.eth)
+
---
+++ common_functions.py
@@ -0,0 +1,116 @@
+# Copyright (c) 2024 Praneeth Vadlapati
+
+import os
+import time
+
+from dotenv import load_dotenv
+import openai
+
+load_dotenv(override=True)  # bypass the cache and reload the variables
+random_state = 1
+data_folder = 'data_files'
+if not os.path.exists(data_folder):
+	os.makedirs(data_folder)
+
+def print_progress(chr='.'):
+	if chr == 0 and type(chr) == int:
+		return
+	if type(chr) == bool:
+		chr = '.' if chr else ','
+	print(chr, end='', flush=True)
+
+def print_error(err=None):
+	# print(err)
+	print_progress('!')
+
+def extract_data(response, tag):
+	if not tag:  # if tags are provided, extract data from tags
+		raise Exception('No data format or tag provided to extract data from the response')
+	response = str(response).strip()  # create a copy
+
+	open_tag = f'<{tag}>'
+	if open_tag not in response:
+		raise Exception(f'Tag "{tag}" not found in the response')
+	start = response.rfind(open_tag) + len(open_tag)
+	
+	close_tag = f'</{tag}>'
+	if close_tag in response[start:]:
+		end = response.find(close_tag, start)
+	else:
+		end = len(response)
+	response = response[start:end].strip()
+
+	if '```csv' in response:
+		response = response.replace('```csv', '```').strip()
+	if '```' in response:
+		response = response.split('```')[1].strip()
+	return response
+
+
+model = os.getenv('LM_MODEL')
+if model:
+	model = model.strip()
+else:
+	raise Exception('LM_MODEL is not set in the environment variables')
+
+client = None
+
+def load_client():
+	global client
+	if client:  # if already loaded, and reloading now
+		print('Reloading client...')
+	load_dotenv(override=True)
+	base_url = os.getenv('LM_PROVIDER_BASE_URL').strip() or None
+
+	api_key = os.getenv('LM_API_KEY').strip() or None
+	client = openai.OpenAI(base_url=base_url, api_key=api_key)
+
+
+load_client()  # Load client for the first time
+
+
+
+def get_lm_response(messages, max_retries=4):
+	if isinstance(messages, str):
+		messages = [{'role': 'user', 'content': messages}]
+
+	for _ in range(max_retries):
+		response = None
+		try:
+			response = client.chat.completions.create(messages=messages, model=model)
+			response = response.choices[0].message.content.strip()
+			if not response:
+				raise Exception('Empty response from the bot')
+			return response
+		except Exception as e:
+			e = str(e)
+			if '429' in e or 'Resource has been exhausted' in e:  # Rate limit
+				total_wait_time = None
+				if 'Please retry after' in e:  # Please retry after X sec
+					total_wait_time = e.split('Please retry after')[1].split('sec')[0].strip()
+					# print(f'Rate Limit reached. Waiting for {total_wait_time} seconds. ', end='')
+					total_wait_time = int(total_wait_time) + 1
+				elif 'Please try again in' in e:  # 'Please try again in 23m3s. ...'
+					rate_limit_time = e.split('Please try again in')[1].split('.')[0].strip()
+					# print(f'Rate Limit reached for {rate_limit_time}. ', end='', flush=True)
+	 				# rate_limit_time = '1m20s'
+					rate_limit_time_min = 0
+					rate_limit_time_sec = 0
+					if 'm' in rate_limit_time:
+						rate_limit_time_min = rate_limit_time.split('m')[0]
+						rate_limit_time = rate_limit_time.split('m')[1]  # get text after 'm'
+					if 's' in rate_limit_time:
+						rate_limit_time_sec = rate_limit_time.split('s')[0]
+					total_wait_time = (int(rate_limit_time_min) * 60) + int(rate_limit_time_sec) + 1
+				else:
+					total_wait_time = 20
+				print_progress(f' RL Wait{total_wait_time}s ')
+				time.sleep(int(total_wait_time))
+			elif '503' in e:  # Service Unavailable
+				print_progress('Unavailable Wait ')
+				time.sleep(15)
+			elif e == 'Connection error.':
+				print_progress('Server not online ')
+			else:
+				print_progress(f'Error Retrying ')
+	raise Exception(f'No response from the bot after {max_retries} retries')
